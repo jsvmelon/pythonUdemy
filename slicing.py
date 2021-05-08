d7384334412f87a0f11496204fac810deb266183
Jean-Sacha Melon <jsmelon@gmail.com>
2021-05-08 19:00:24 +0100

    simple exercise slicing strings backwards

---
+++ slicing.py
@@ -0,0 +1,8 @@
+#                    1         2
+#          012345678901234567890123456
+letters = 'abcdefghijklmnopqrstuvwxyz'
+
+# challenge
+print(letters[16:13:-1])
+print(letters[4:-27:-1])
+print(letters[26:-9:-1])
