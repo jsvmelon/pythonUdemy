5eba1c12f43a35d6e85d76d26ce1d7f5ed584f9e
Jean-Sacha Melon <jsmelon@gmail.com>
2021-05-08 22:44:24 +0100

    old style string formatting

---
+++ interpolation.py
@@ -0,0 +1,10 @@
+age = 24
+print("My age is %d years" % age)
+
+major = "years"
+minor = "months"
+print("My age is %d %s, %d %s" % (age, major, 6, minor))
+print("PI is approx %f" % (22/7))
+print("PI is approx %60.50f" % (22/7))
+print("PI in hex is approx %x" % (22))
+
