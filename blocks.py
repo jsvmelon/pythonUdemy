e8c91c95ed1ba18fd0a65e4c1bb75ad5ad0c5cc9
Jean-Sacha Melon <jsmelon@gmail.com>
2021-05-08 23:12:26 +0100

    adding elif and trying the debugger

--- blocks.py
+++ blocks.py
@@ -7,6 +7,16 @@ if age >= 18:
     print("Please put an X in the box")
 else:
     print("Your are not old enough to vote")
+    print("Please come back in {0} years".format(18-age))
+
+if age < 18:
+    print("Your are not old enough to vote")
+    print("Please come back in {0} years".format(18 - age))
+elif age == 900:
+    print("Sorry Yoda, you die in the Return of the Jedi")
+else:
+    print("You are old enough to vote")
+    print("Please put an X in the box")
 
 
 # for i in range(1, 5):
