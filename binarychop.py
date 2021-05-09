796cbbea83b947558bfd947b7f5d141b3363168d
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-05-09 22:51:52 +0100

    refactor: moved the increase of guesses to another location to avoid repetition

--- binarychop.py
+++ binarychop.py
@@ -1,11 +1,12 @@
 low = 1
 high = 1000
-guesses = 1
+guesses = 0
 
 print("Please think of a number between {} and {}".format(low, high))
 input("Press ENTER to start")
 
 while True:
+    guesses += 1
     guess = low + (high-low) // 2
     high_low = input("My guess is {}. Should I guess higher or lower? "
                      "Enter h or l, or c if my guess was correct ".
@@ -14,11 +15,9 @@ while True:
     if high_low == "h":
         # guess higher
         low = guess + 1
-        guesses += 1
     elif high_low == "l":
         # guess lower
         high = guess - 1
-        guesses += 1
     elif high_low == "c":
         # correct guess
         print("yay! Got it in {} guesses".format(guesses))
