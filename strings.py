405bde43a8331454db18f5a118969ceef6eddea0
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-05-09 21:28:24 +0100

    string fundamentals

---
+++ strings.py
@@ -0,0 +1,24 @@
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's strings are easy to use")
+print('We cna even include "quotes" in strings')
+print("hello" + " world")
+greeting = "Hello"
+name = "Tim"
+#input("Please enter you name ")
+print(greeting + name)
+
+print(greeting + ' ' + name)
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age_in_words = "2 years"
+print(age, type(age))
+
+# results in an error
+print(name + " is " + age + " years old")
+
