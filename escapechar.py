53135f508e24b061a0a7f8f33c43da70ec687266
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-05-09 23:04:08 +0100

    corrected to conform to PEP8

--- escapechar.py
+++ escapechar.py
@@ -1,8 +1,8 @@
-splitString = "This string has been\nsplit over\nseveral\nlines"
-print(splitString)
+split_string = "This string has been\nsplit over\nseveral\nlines"
+print(split_string)
 
-tabString = "1\t2\t3\t4\t5"
-print(tabString)
+tab_string = "1\t2\t3\t4\t5"
+print(tab_string)
 
 print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
 # or
@@ -11,12 +11,12 @@ print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
 print("""The pet shop owner said "No, no, \
  'e's uh,...he's resting".""")
 
-anotherSplitstring = """This string has been \
+another_splitstring = """This string has been \
 split over \
 several \
 lines"""
 
-print(anotherSplitstring)
+print(another_splitstring)
 
 print("C:\\Users\\timbuchalka\\notes.txt")
 print(r"C:\Users\timbuchalka\notes.txt")
