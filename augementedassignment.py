a5e9a05c2de26a41cb2c894d838911eff1ed8787
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-05-09 22:52:20 +0100

    some simple examples for using augmented assignment in python

---
+++ augementedassignment.py
@@ -0,0 +1,22 @@
+x = 1
+print(x)
+
+x += 1
+print(x)
+
+x *= 3
+print(x)
+
+x //= 2
+print(x)
+
+x **= 2
+print(x)
+
+x %= 5
+print(x)
+
+greeting = "Good "
+greeting += "morning "
+greeting *= 5
+print(greeting)
