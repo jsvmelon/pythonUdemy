258dedefba296ef347954a36e7d2e01d4edb6f12
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-05-09 21:26:18 +0100

    basic operators

---
+++ operators.py
@@ -0,0 +1,23 @@
+a = 12
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)   # 4.0
+print(a // b)  # 4 integer division, rounded down towards minus infinity
+print(a % b)   # 0
+
+print()
+
+for i in range(1, a//b):
+    print(i)
+
+r = range(1, 4)
+print(r)
+
+bun_price = 2.40
+money = 15
+print(money // bun_price)
+
+print(a + b / 3 - 4 * 12)
+print((((a + b) / 3) - 4) * 12)
