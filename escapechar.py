a31b3d1b6468ce42c7c3e1900e880be8d26306b0
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-05-09 21:26:07 +0100

    some fundamental escape chars

---
+++ escapechar.py
@@ -0,0 +1,22 @@
+splitString = "This string has been\nsplit over\nseveral\nlines"
+print(splitString)
+
+tabString = "1\t2\t3\t4\t5"
+print(tabString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+# or
+print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
+# or
+print("""The pet shop owner said "No, no, \
+ 'e's uh,...he's resting".""")
+
+anotherSplitstring = """This string has been \
+split over \
+several \
+lines"""
+
+print(anotherSplitstring)
+
+print("C:\\Users\\timbuchalka\\notes.txt")
+print(r"C:\Users\timbuchalka\notes.txt")
