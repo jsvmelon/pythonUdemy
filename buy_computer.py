1a86ec812c18bcf5130637be2dacdf5246b79a6d
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-05-12 23:08:38 +0100

    adding removing parts from the chosen list also refactored to print the menu whenever a wrong choice is made

--- buy_computer.py
+++ buy_computer.py
@@ -1,18 +1,22 @@
-available_choices = ["computer", "monitor", "keyboard", "mouse", "mouse mat", "hdmi"]
+available_choices = ["computer", "monitor", "keyboard", "mouse", "mouse mat", "hdmi", "dvd drive"]
 current_choice = -1
 computer_parts = []
 
-# print the menu
-print("Please add options from the list below:")
-for number, part in enumerate(available_choices):
-    print("{}: {}".format(number+1, part))
-
 while current_choice != 0:
     if 1 <= current_choice <= len(available_choices):
-        print("Adding {}".format(available_choices[current_choice - 1]))
-        computer_parts.append(available_choices[current_choice - 1])
-        current_choice = -1
+        chosen_item = available_choices[current_choice-1]
+        if chosen_item in computer_parts:
+            print("Removing {} from your list".format(chosen_item))
+            computer_parts.remove(chosen_item)
+        else:
+            print("Adding {}".format(chosen_item))
+            computer_parts.append(chosen_item)
     else:
-        current_choice = int(input())
+        # print the menu
+        print("Please add options from the list below:")
+        for number, part in enumerate(available_choices):
+            print("{}: {}".format(number+1, part))
+
+    current_choice = int(input())
 
 print(computer_parts)
