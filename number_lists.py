4f82daa198d757891a5496237c35dc5d33eb7e2f
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-05-12 22:42:58 +0100

    some checks to see that lists are mutable and integers are not

---
+++ number_lists.py
@@ -0,0 +1,16 @@
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+print(min(even), max(even))
+
+print(min(odd), max(odd))
+
+print()
+print(len(even))
+print(len(odd))
+
+print()
+print("mississippi".count("iss"))
+
+even.append(10)
+print(even)
