2d960bce30550010ee401e6a1f1d8842f6f9ba66
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-05-13 20:37:01 +0100

    methods for creating lists

--- number_lists.py
+++ number_lists.py
@@ -1,22 +1,41 @@
+empty_list = []
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-even.extend(odd)
-another_even = even
-even.sort(reverse=True)
-print(even)
-print(another_even)
+numbers = even + odd
+sorted_numbers = (sorted(numbers))
 
-print(min(even), max(even))
+print(numbers)
+print(sorted_numbers)
 
-print(min(odd), max(odd))
+digits = list("41243241345688566")
+print(digits)
 
-print()
-print(len(even))
-print(len(odd))
+# more_numbers = list(numbers)
+# more_numbers = numbers[:]
+more_numbers = numbers.copy()
 
-print()
-print("mississippi".count("iss"))
+print(more_numbers)
 
-even.append(10)
-print(even)
+print((numbers is more_numbers))
+print(numbers == more_numbers)
+
+# even.extend(odd)
+# another_even = even
+# even.sort(reverse=True)
+# print(even)
+# print(another_even)
+#
+# print(min(even), max(even))
+#
+# print(min(odd), max(odd))
+#
+# print()
+# print(len(even))
+# print(len(odd))
+#
+# print()
+# print("mississippi".count("iss"))
+#
+# even.append(10)
+# print(even)
