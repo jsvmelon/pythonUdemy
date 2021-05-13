51f287809012e98a2a529f0f9f41975d0c607ee8
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-05-13 20:26:33 +0100

    playing with some sorting

--- number_lists.py
+++ number_lists.py
@@ -1,6 +1,12 @@
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
+even.extend(odd)
+another_even = even
+even.sort(reverse=True)
+print(even)
+print(another_even)
+
 print(min(even), max(even))
 
 print(min(odd), max(odd))
