f93c67b2297ff35d6762df68c682695fd326dfbc
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-05-13 21:24:16 +0100

    delete outliers from a data list, this time avoiding problems with the loops index when deleting

--- outliers.py
+++ outliers.py
@@ -1,14 +1,34 @@
 data = [4, 5, 104, 105, 110, 120, 130, 150, 160, 170, 183, 185, 186, 187,
         188, 191, 350, 360]
-# del data[0:2]
-# print(data)
 
 min_valid = 100
 max_valid = 200
 
-# this does not work
+# process the low values first
+stop = 0
 for index, value in enumerate(data):
-    if not (min_valid < value < max_valid):
-        del data[index]
+    if value >= min_valid:
+        stop = index
+        break
 
+del data[:stop]
 print(data)
+
+stop = -1
+while data[stop] >= max_valid:
+    stop -= 1
+
+print(stop)
+del data[stop:]
+print(data)
+
+# del data[0:2]
+# print(data)
+
+#
+# # this does not work
+# for index, value in enumerate(data):
+#     if not (min_valid < value < max_valid):
+#         del data[index]
+#
+# print(data)
