9d20cf6f41ef5fa2a7eb8e5b533a74d73f31e89c
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-05-20 23:10:48 +0100

    using unpacking for clearer code

--- tuples.py
+++ tuples.py
@@ -16,3 +16,16 @@ print(t2)
 
 # list is mutable
 t2[0] = "fff"
+
+first, second, third = t
+print(first)
+print(second)
+print(third)
+
+table = ("Coffee table", 200, 100, 75, 34.50)
+print(table[1] * table[2])
+
+# using unpacking we can work with names instead
+name, length, width, height, price = table
+print(length * width)
+
