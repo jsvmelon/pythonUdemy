6bf9035c1da986b0364ca5e3806228cb4d16a59d
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-05-20 22:35:27 +0100

    playing with split

--- splitting_things.py
+++ splitting_things.py
@@ -5,3 +5,14 @@ print(words)
 
 numbers = "9,223,123,46,3354,463,34455,33"
 print(numbers.split(","))
+
+separators = ",.;: "
+values = "".join(char if char not in separators else " " for char in numbers).split()
+print(values)
+
+# mini challenge
+integer_values = []
+for i in values:
+    integer_values.extend([int(i)])
+
+print(integer_values)
