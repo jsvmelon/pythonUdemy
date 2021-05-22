eec376de35e54296e7a5922a103ac7e75dd99b5e
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-05-22 22:26:35 +0100

    importing data and an exercise using and displaying nested data

---
+++ jukebox_menu.py
@@ -0,0 +1,34 @@
+from album_data import albums
+
+
+def choose_album():
+    print("Please choose your album (invalid choice exits):")
+    for index, (title, artist, year, songs) in enumerate(albums):
+        print("{}: {}".format(index + 1, title))
+
+    return int(input()) - 1  # return 0 based result
+
+
+def choose_song(album_choice):
+    print("Please choose your song (invalid choice exits):")
+    for index, (track_number, song) in enumerate(albums[album_choice][SONGS_LIST_INDEX]):
+        print("{}: {}".format(track_number, song))
+
+    return int(input()) - 1  # return 0 based result
+
+
+SONGS_LIST_INDEX = 3
+SONG_TITLE_INDEX = 1
+
+while True:
+    album_choice = choose_album()
+    if 0 <= album_choice < len(albums):
+        song_choice = choose_song(album_choice)
+        if 0 <= song_choice < len(albums[album_choice][SONGS_LIST_INDEX]):
+            print("Playing {} ...\n".format(albums[album_choice][SONGS_LIST_INDEX][song_choice][SONG_TITLE_INDEX]))
+    else:
+        break
+
+    # print(album_choice)
+    # print(albums[album_choice - 1])
+    # print(songs_list)
