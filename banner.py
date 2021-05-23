8ca541e85de7fa6855df43fb346025114be2a03d
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-05-23 22:32:11 +0100

    Added a docstring to the banner

--- banner.py
+++ banner.py
@@ -1,4 +1,11 @@
-def banner(text, width=66):
+def banner(text=" ", width=66):
+    """
+    Function that prints text given inside a surrounding banner of '**'
+    Without any parameter a surrounded empty line will be printed.
+    :param text: The text to be printed. Default is the empty string.
+    :param width: The width of a banner line, default is 66
+    :return: None
+    """
     if len(text) > width - 4:
         raise ValueError("String {} is larger than specified width {}".
                          format(text, width))
@@ -12,7 +19,7 @@ def banner(text, width=66):
 
 banner("*")
 banner("Hello dude")
-banner(" ")
+banner(width=70)
 banner("Hello dudes")
 banner("F" * 60)
 banner("*")
