b55a95042b3d831a2211100a359526c210430bb8
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-07-14 21:16:27 +0100

    OO in python: (1) how to define a class, (2) how to instantiate it, (3) using a method on the class with an instance as parameter, adding an attribute to a specific instance but not the class

--- oo/oop.py
+++ oo/oop.py
@@ -11,6 +11,18 @@ class Kettle(object):
         self.price = price
         self.on = False
 
+    def switch_on(self):
+        if self.on:
+            print("The kettle is already on")
+        else:
+            self.on = True
+
+    def switch_off(self):
+        if not self.on:
+            print("The kettle is already off")
+        else:
+            self.on = False
+
 
 kenwood = Kettle("Kenwood", 8.99)
 print(kenwood.make, kenwood.price)
@@ -22,3 +34,26 @@ hamilton = Kettle("Hamilton", 14.55)
 print(hamilton.make, hamilton.price)
 
 print("Models: {} = {}, {} = {}".format(kenwood.make, kenwood.price, hamilton.make, hamilton.price))
+
+# a little bit more readable
+print("Models: {0.make} = {0.price}, {1.make} = {1.price}".format(kenwood, hamilton))
+
+# using the instance
+print(hamilton.on)
+hamilton.switch_on()
+print(hamilton.on)
+
+# alternatively we can use the class method with an instance as parameter
+print(kenwood.on)
+Kettle.switch_on(kenwood)
+print(kenwood.on)
+kenwood.switch_on()
+
+print("*" * 80)
+
+# objects are dynamic - we can even add an attribute without defining it in the class definition
+kenwood.power = 1.5
+print(kenwood.power)
+
+# hamilton does not have the attribute ...
+# print(hamilton.power)
