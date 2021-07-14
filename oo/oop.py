0667dcf495675046764804abcd07479f97639570
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-07-14 19:55:04 +0100

    Simple object example

---
+++ oo/oop.py
@@ -0,0 +1,24 @@
+# a = 12
+# b = 4
+# print(a+b)
+# print(a.__add__(b))
+
+
+class Kettle(object):
+
+    def __init__(self, make, price):
+        self.make = make
+        self.price = price
+        self.on = False
+
+
+kenwood = Kettle("Kenwood", 8.99)
+print(kenwood.make, kenwood.price)
+
+kenwood.price = 12.75
+print(kenwood.price)
+
+hamilton = Kettle("Hamilton", 14.55)
+print(hamilton.make, hamilton.price)
+
+print("Models: {} = {}, {} = {}".format(kenwood.make, kenwood.price, hamilton.make, hamilton.price))
