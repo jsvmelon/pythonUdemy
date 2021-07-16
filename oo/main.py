b433f3c14ecb0591c6155aadbaa48f26f9706253
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-07-16 14:52:53 +0100

    Getters and setters

---
+++ oo/main.py
@@ -0,0 +1,18 @@
+from player import Player
+
+js = Player("JS")
+
+print(js.name)
+print(js.lives)
+
+js.lives -= 1
+print(js)
+
+js.lives -= 1
+print(js)
+
+js.lives -= 1
+print(js)
+
+js.lives -= 1
+print(js)
