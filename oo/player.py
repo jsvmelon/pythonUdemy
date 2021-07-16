c25ae11ef2386f739e9f1d3c56f1a1ec2412cd9c
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-07-16 21:29:38 +0100

    implemented another getter and setter with property

--- oo/player.py
+++ oo/player.py
@@ -3,8 +3,22 @@ class Player(object):
     def __init__(self, name):
         self.name = name
         self._lives = 3
-        self.level = 1
-        self.score = 0
+        self._level = 1
+        self._score = 0
+
+    def _get_level(self):
+        return self._level
+
+    def _set_level(self, level):
+        if level < 1:
+            print("Can't go lower than level 1 - setting to 1")
+            level = 1
+
+        diff = level - self._level
+        self._level = level
+        self._score += (1000 * diff)
+
+    level = property(_get_level, _set_level)
 
     def _get_lives(self):
         return self._lives
@@ -20,3 +34,11 @@ class Player(object):
 
     def __str__(self):
         return "Name: {0.name}, Lives: {0.lives}, Level: {0.level}, Score {0.score}".format(self)
+
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, score):
+        self._score = score
