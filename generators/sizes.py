e86a98275b495a9dedbb35ae219661f7537b0415
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-07-19 15:07:10 +0100

    Using next on the generator and see the effects of it on a generator and on the 'iterable' range().

--- generators/sizes.py
+++ generators/sizes.py
@@ -10,19 +10,24 @@ def my_range(n: int):
         start += 1
 
 
-_ = input("line 13")
-big_range = my_range(5)
+# big_range = my_range(5)
+big_range = range(5)
+# _ = input("line 14")
 
-_ = input("line 16")
+# print(next(big_range))
 print("big range is {} bytes".format(sys.getsizeof(big_range)))
 
 # create a list containing all the values in big range
 big_list = []
-_ = input("line 21")
+# _ = input("line 20")
 for value in big_range:
-    _ = input("line 23")
+    # _ = input("line 22")
     big_list.append(value)
 
 print("big list is {} bytes".format(sys.getsizeof(big_list)))
 print(big_range)
 print(big_list)
+
+print("looping again ... or not")
+for i in big_range:  # the values in big_range are exhausted, so nothing happens here
+    print("i is {}".format(i))
