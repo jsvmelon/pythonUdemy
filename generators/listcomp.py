9e3e4d4ae5c2b9b9a8463e2434b44369a08faa95
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-07-20 15:06:39 +0100

    Exercises covering the basics of list comprehensions

--- generators/listcomp.py
+++ generators/listcomp.py
@@ -2,6 +2,9 @@ print(__file__)
 
 numbers = [1, 2, 3, 4, 5, 6]
 
-squares = [number ** 2 for number in range(1, 11)]
+number = int(input("Please enter a number: "))
 
-print(squares)
+squares = [number ** 2 for number in range(1, 11)]  # this code is NOT modifying the value of 'number' in the out scope
+
+index_pos = numbers.index(number)
+print(squares[index_pos])
