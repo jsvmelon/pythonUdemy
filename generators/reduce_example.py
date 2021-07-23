04157d5850a204d97560001428ad7e4cf779cd11
jean-sacha.melon <jean-sacha.melon@smarkets.com>
2021-07-23 13:49:01 +0100

    Introducing reduce()

---
+++ generators/reduce_example.py
@@ -0,0 +1,13 @@
+import functools
+import timeit
+
+
+def calc_values(x, y: int):
+    return x * y
+
+
+numbers = [2, 3, 5, 8, 13]
+
+reduced_value = functools.reduce(calc_values, numbers)
+print(reduced_value)
+
